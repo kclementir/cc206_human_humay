537e40c248f0c353e6cfb046d778bb3eea3108c4
kclementir <kianiclementir@gmail.com>
2023-11-24 04:25:05 +0800

    updated login

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:cc206_human_humay/components/tabs.dart';
+import 'login_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: TabBarDemo(),
+      home: LoginPage(),
     );
   }
 }
