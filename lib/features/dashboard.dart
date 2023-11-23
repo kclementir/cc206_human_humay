77890604e4c2e39fb39d84ee8590626a02a5028a
kclementir <kianiclementir@gmail.com>
2023-11-24 03:12:30 +0800

    updated content

--- lib/features/dashboard.dart
+++ lib/features/dashboard.dart
@@ -5,76 +5,112 @@ class Dashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          //Weather
-          Card(
-            margin: EdgeInsets.all(16.0),
-            child: ListTile(
-              title: Text('Weather'),
-              subtitle: Text('Current weather information'),
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'HUMÁNHUMAY',
+          style: TextStyle(
+            fontFamily: 'Brand',
+            color: Color(0xFF367750),
+            fontWeight: FontWeight.bold,
           ),
-
-          //Crop Analysis
-          Card(
-            margin: EdgeInsets.all(16.0),
-            child: ListTile(
-              title: Text('Crop Analysis'),
-              subtitle: Text('Crop analysis data'),
-            ),
+        ),
+        backgroundColor: Colors.white,
+      ),
+      // body: Container(
+      //   constraints: const BoxConstraints.expand(),
+      //   decoration: const BoxDecoration(
+      //     image: DecorationImage(
+      //       image: AssetImage('assets/images/bg2.png'),
+      //       fit: BoxFit.fitWidth,
+      //     ),
+      //   ),
+      //Background Color
+      body: Container(
+        constraints: const BoxConstraints
+            .expand(), // Ensure the container takes up the whole screen
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF367750),
+              Color(0xFF8B8421),
+            ],
           ),
-
-          //Analysis History
-          Card(
-            margin: EdgeInsets.all(16.0),
-            child: ListTile(
-              title: Text('History of Analysis'),
-              subtitle: Text('Analysis history data'),
-            ),
-          ),
-
-          Row(
+        ),
+        child: const SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              //Cultivation Tips
-              Expanded(
-                child: Card(
-                  margin: EdgeInsets.all(16.0),
-                  child: ListTile(
-                    title: Text('Cultivation Tips'),
-                    subtitle: Text('Helpful tips for cultivation'),
-                  ),
+              // Weather
+              Card(
+                margin: EdgeInsets.all(16.0),
+                child: ListTile(
+                  title: Text('Weather'),
+                  subtitle: Text('Current weather information'),
                 ),
               ),
 
-              //Pests and Diseases
-              Expanded(
-                child: Card(
-                  margin: EdgeInsets.all(16.0),
-                  child: ListTile(
-                    title: Text('Pests and Diseases Info'),
-                    subtitle: Text('Information on common pests and diseases'),
-                  ),
+              // Crop Analysis
+              Card(
+                margin: EdgeInsets.all(16.0),
+                child: ListTile(
+                  title: Text('Crop Analysis'),
+                  subtitle: Text('Crop analysis data'),
                 ),
               ),
 
-              //Crop Yield Detection
-              Expanded(
-                child: Card(
-                  margin: EdgeInsets.all(16.0),
-                  child: ListTile(
-                    title: Text('Crop Yield Detection'),
-                    subtitle: Text('Crop yield data and analysis'),
-                  ),
+              // Analysis History
+              Card(
+                margin: EdgeInsets.all(16.0),
+                child: ListTile(
+                  title: Text('History of Analysis'),
+                  subtitle: Text('Analysis history data'),
                 ),
+              ),
+
+              Row(
+                children: [
+                  // Cultivation Tips
+                  Expanded(
+                    child: Card(
+                      margin: EdgeInsets.all(16.0),
+                      child: ListTile(
+                        title: Text('Cultivation Tips'),
+                        subtitle: Text('Helpful tips for cultivation'),
+                      ),
+                    ),
+                  ),
+
+                  // Pests and Diseases
+                  Expanded(
+                    child: Card(
+                      margin: EdgeInsets.all(16.0),
+                      child: ListTile(
+                        title: Text('Pests and Diseases Info'),
+                        subtitle:
+                            Text('Information on common pests and diseases'),
+                      ),
+                    ),
+                  ),
+
+                  // Crop Yield Detection
+                  Expanded(
+                    child: Card(
+                      margin: EdgeInsets.all(16.0),
+                      child: ListTile(
+                        title: Text('Crop Yield Detection'),
+                        subtitle: Text('Crop yield data and analysis'),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
-        ],
+        ),
       ),
-    ));
+    );
   }
 }
--- lib/features/user.dart
+++ lib/features/user.dart
@@ -5,33 +5,72 @@ void main() {
 }
 
 class User extends StatelessWidget {
-  const User({super.key});
+  const User({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'HUMÁNHUMAY',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('HUMÁNHUMAY'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset('assets/images/logo.png'),
-              const Text('Palaydin'),
-              const Text('How\'s your experience with HUMÁNHUMAY?'),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text('Let\'s grow rice crops together!'),
-              ),
-            ],
+          title: const Text(
+            'HUMÁNHUMAY',
+            style: TextStyle(
+              fontFamily: 'Brand',
+              color: Color(0xFF367750),
+              fontWeight: FontWeight.bold,
+            ),
           ),
+          backgroundColor: Colors.white,
         ),
-      ),
+        //Background Color
+        body: Container(
+          constraints: const BoxConstraints
+              .expand(), // Ensure the container takes up the whole screen
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color(0xFF367750),
+                Color(0xFF8B8421),
+              ],
+            ),
+          ),
+          child: const SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                //User Profile
+                Card(
+                  margin: EdgeInsets.all(16.0),
+                  child: ListTile(
+                    title: Text('User Profile'),
+                    subtitle: Text('Manage Account'),
+                  ),
+                ),
+
+                //Feeback
+                Card(
+                  margin: EdgeInsets.all(16.0),
+                  child: ListTile(
+                    title: Text("How's your experience with HUMÁNHUMAY?"),
+                    subtitle: Text('Give Feedback'),
+                  ),
+                ),
+
+                // Share
+                Card(
+                  margin: EdgeInsets.all(16.0),
+                  child: ListTile(
+                    title: Text("Let's grow rice crops together!"),
+                    subtitle: Text('Share'),
+                  ),
+                ),
+              ], // Close the children list
+            ), // Close the Column
+          ), // Close the SingleChildScrollView
+        ), // Close the Container
+      ), // Close the Scaffold
     );
   }
 }
