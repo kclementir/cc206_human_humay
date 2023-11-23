e24c0e9b85fe5c015a70f41712fedc2cde120ef4
kclementir <kianiclementir@gmail.com>
2023-11-24 03:43:27 +0800

    refractor code
    changed MyApp to LoginPage to integrate into main.dart file

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,87 +1,74 @@
 import 'package:flutter/material.dart';
 
-void main() {
- runApp(const MyApp());
-}
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
 
-class MyApp extends StatelessWidget {
- const MyApp({super.key});
-
- @override
- Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Login Form',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Login Form'),
-    );
- }
-}
-
-class MyHomePage extends StatefulWidget {
- const MyHomePage({Key? key, required this.title}) : super(key: key);
-
- final String title;
-
- @override
- // ignore: library_private_types_in_public_api
- _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
- final _formKey = GlobalKey<FormState>();
-
- @override
- Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Flutter Login Form'),
       ),
-      body: Center(
+      body: const Center(
         child: SingleChildScrollView(
-          padding: const EdgeInsets.all(16),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                TextFormField(
-                 decoration: const InputDecoration(
-                    hintText: 'Enter your email',
-                 ),
-                 validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter some text';
-                    }
-                    return null;
-                 },
-                ),
-                const SizedBox(height: 16),
-                TextFormField(
-                 decoration: const InputDecoration(
-                    hintText: 'Enter your password',
-                 ),
-                 validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter some text';
-                    }
-                    return null;
-                 },
-                 obscureText: true,
-                ),
-                const SizedBox(height: 16),
-                ElevatedButton(
-                 onPressed: () {
-                   // TODO: login logic here
-                 },
-                 child: const Text('Submit'),
-                ),
-              ],
-            ),
-          ),
+          padding: EdgeInsets.all(16),
+          child: LoginForm(),
         ),
       ),
     );
- }
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({Key? key}) : super(key: key);
+
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Enter your email',
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 16),
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Enter your password',
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+            obscureText: true,
+          ),
+          const SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {
+              // TODO: login logic here
+            },
+            child: const Text('Submit'),
+          ),
+        ],
+      ),
+    );
+  }
 }
