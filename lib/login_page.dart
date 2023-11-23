bd2ae42153995855003a0a7372d9e71409b5be2e
kclementir <kianiclementir@gmail.com>
2023-11-24 05:57:46 +0800

    modified

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cc206_human_humay/components/tabs.dart';
+import 'package:cc206_human_humay/features/signup.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -20,6 +21,7 @@ class LoginPage extends StatelessWidget {
             ],
           ),
         ),
+
         //App Logo
         child: Center(
           child: SingleChildScrollView(
@@ -32,8 +34,9 @@ class LoginPage extends StatelessWidget {
                   width: 200,
                   height: 200,
                 ),
-                const SizedBox(height: 100),
-                const LoginForm(),
+                const SizedBox(
+                    height: 100), // space between img and Login widget
+                const Login(title: 'Flutter Login'), // display login form
               ],
             ),
           ),
@@ -43,62 +46,97 @@ class LoginPage extends StatelessWidget {
   }
 }
 
-class LoginForm extends StatefulWidget {
-  const LoginForm({Key? key}) : super(key: key);
+class Login extends StatelessWidget {
+  const Login({Key? key, required this.title}) : super(key: key);
 
-  @override
-  _LoginFormState createState() => _LoginFormState();
-}
+  final String title;
 
-class _LoginFormState extends State<LoginForm> {
-  final _formKey = GlobalKey<FormState>();
+  void _toggleSignUp(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const SignUpPage()),
+    );
+  }
+
+  Widget _login(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        TextFormField(
+          decoration: const InputDecoration(
+            labelText: 'Username',
+            labelStyle: TextStyle(color: Colors.white),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            hintStyle: TextStyle(color: Colors.white),
+          ),
+          style: const TextStyle(color: Colors.white),
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            labelText: 'Password',
+            labelStyle: TextStyle(color: Colors.white),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            hintStyle: TextStyle(color: Colors.white),
+          ),
+          obscureText: true,
+          style: const TextStyle(color: Colors.white),
+        ),
+        const SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: () {
+            // Handle login
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const TabBarDemo()),
+            );
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color(0xFF367750),
+          ),
+          child: const Text('Login', style: TextStyle(color: Colors.white)),
+          // Set button text color
+        ),
+        const SizedBox(height: 10),
+        TextButton(
+          onPressed: () {
+            // Toggle to sign up form
+            _toggleSignUp(context);
+          },
+          child: RichText(
+            text: const TextSpan(
+              text: 'Don\'t have an account? ',
+              style: TextStyle(
+                color: Colors.white,
+                // Set text color for the first part
+              ),
+              children: [
+                TextSpan(
+                  text: 'Sign up',
+                  style: TextStyle(
+                    color: Color(0xFFFFC926),
+                    // Set text color for the word "Sign up"
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          TextFormField(
-            decoration: const InputDecoration(
-              hintText: 'Enter your email',
-            ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-          ),
-          const SizedBox(height: 16),
-          TextFormField(
-            decoration: const InputDecoration(
-              hintText: 'Enter your password',
-            ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-            obscureText: true,
-          ),
-          const SizedBox(height: 16),
-          ElevatedButton(
-            onPressed: () {
-              if (_formKey.currentState?.validate() ?? false) {
-                // If form is valid, navigate to the Dashboard
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const TabBarDemo()),
-                );
-              }
-            },
-            child: const Text('Submit'),
-          ),
-        ],
-      ),
-    );
+    return _login(context);
   }
 }
