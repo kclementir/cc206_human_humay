89b21648a5328bf6d4dd6ae56eae6efd55b299cd
kclementir <kianiclementir@gmail.com>
2023-12-05 23:26:31 +0800

    update

--- lib/features/community.dart
+++ lib/features/community.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:cc206_human_humay/features/writepost.dart';
 
 void main() {
-  runApp(const Community());
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Community(),
+  ));
 }
 
 class Community extends StatelessWidget {
@@ -10,108 +13,127 @@ class Community extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'HUMÁNHUMAY',
-            style: TextStyle(
-              fontFamily: 'Brand',
-              color: Color(0xFF367750),
-              fontWeight: FontWeight.bold,
-            ),
+      home: TabBarDemo(),
+    );
+  }
+}
+
+class TabBarDemo extends StatelessWidget {
+  const TabBarDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'HUMÁNHUMAY',
+          style: TextStyle(
+            fontFamily: 'Brand',
+            color: Color(0xFF367750),
+            fontWeight: FontWeight.bold,
           ),
-          backgroundColor: Colors.white,
-          actions: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: IconButton(
-                icon: const Icon(Icons.share, color: Colors.green),
-                onPressed: () {
-                  // Add your share button logic here
-                },
-              ),
-            ),
-          ],
         ),
-        body: Container(
-          constraints: const BoxConstraints.expand(),
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color(0xFF367750),
-                Color(0xFF8B8421),
-              ],
+        backgroundColor: Colors.white,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: IconButton(
+              icon: const Icon(Icons.share, color: Color(0xFF367750)),
+              onPressed: () {
+                // Add your share button logic here
+              },
             ),
           ),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ListView(
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        height: 30,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(12.0),
+        ],
+      ),
+      body: Container(
+        constraints: const BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF367750),
+              Color(0xFF8B8421),
+            ],
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 30,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: const TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Search Community',
+                          hintStyle: TextStyle(color: Colors.grey),
+                          border: InputBorder.none,
+                          prefixIcon: Icon(Icons.search, color: Colors.grey),
                         ),
-                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                        child: const TextField(
-                          decoration: InputDecoration(
-                            hintText: 'Search Community',
-                            hintStyle: TextStyle(color: Colors.grey),
-                            border: InputBorder.none,
-                            prefixIcon: Icon(Icons.search, color: Colors.grey),
-                          ),
-                          style: TextStyle(color: Colors.black),
-                        ),
+                        style: TextStyle(color: Colors.black),
                       ),
                     ),
-                    const SizedBox(width: 10),
-                    const Icon(Icons.notifications, color: Colors.white),
-                    const SizedBox(width: 10),
-                    const Icon(Icons.more_vert, color: Colors.white),
-                  ],
-                ),
+                  ),
+                  const SizedBox(width: 10),
+                  const Icon(Icons.notifications, color: Colors.white),
+                  const SizedBox(width: 10),
+                  const Icon(Icons.more_vert, color: Colors.white),
+                ],
+              ),
 
-                const SizedBox(height: 20),
+              const SizedBox(height: 20),
 
-                // Adding three Card widgets with different images below
-                _buildPostCard(
-                  'Job Distor',
-                  'Good Morning mga kababayan! Magtatanong lang sana ako kung anong klasing peste ito saming lupain?',
-                  'assets/images/jub.jpg',
-                  'assets/images/usercom1.png',
-                ),
+              // Adding three Card widgets with different images below
+              _buildPostCard(
+                'Job Distor',
+                'Good Morning mga kababayan! Magtatanong lang sana ako kung anong klasing peste ito saming lupain?',
+                'assets/images/jub.jpg',
+                'assets/images/usercom1.png',
+              ),
 
-                const SizedBox(height: 16),
+              const SizedBox(height: 16),
 
-                _buildPostCard(
-                  'Keben Hokseng',
-                  'Hello... Gus2 q lang itanong kung ano itong yellow sa mga pananim nmin..hehe tnx everyone..',
-                  'assets/images/keben.png',
-                ),
+              _buildPostCard(
+                'Keben Hokseng',
+                'Hello... Gus2 q lang itanong kung ano itong yellow sa mga pananim nmin..hehe tnx everyone..',
+                'assets/images/keben.png',
+                'assets/images/usercom.png',
+              ),
+              const SizedBox(height: 16),
 
-                // Your existing content goes here
-              ],
-            ),
+              _buildPostCard(
+                'Rechi Poll',
+                'Hello pu... Bak8 naging gan2 ang palayan ng lolo q? curious lg pu.',
+                'assets/images/rice.jpg',
+                'assets/images/rechiepol.jpg',
+              ),
+              const SizedBox(height: 16),
+              // Your existing content goes here
+            ],
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const writepost()),
-            );
-          },
-          backgroundColor: Colors.green,
-          child: const Icon(Icons.edit),
-        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const WritePost()),
+          );
+        },
+        backgroundColor: const Color(0xFF367750),
+        child:
+            const Icon(Icons.edit, color: Color.fromARGB(255, 255, 255, 255)),
       ),
     );
   }
@@ -128,35 +150,10 @@ class Community extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    username,
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    postText,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      color: Colors.grey,
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            // Image
             Expanded(
               child: Center(
                 child: Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 16),
-                  height: 250,
-                  width: 300,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(8.0),
                     image: DecorationImage(
@@ -167,29 +164,87 @@ class Community extends StatelessWidget {
                 ),
               ),
             ),
+            // Context
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // User profile icon and Context
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      // User profile icon with image
+                      CircleAvatar(
+                        radius: 20,
+                        backgroundImage: AssetImage(avatarImage),
+                      ),
+                      const SizedBox(width: 8),
+                      // Username and Date posted
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // Username
+                          Text(
+                            username,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 4),
+                          // Date posted
+                          Text(
+                            'Date Posted: ${DateTime.now().toString().split(' ')[0]}',
+                            style: const TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  // Post text
+                  Text(
+                    postText,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      color: Color.fromARGB(255, 0, 0, 0),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            // Like
             const SizedBox(height: 8),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.thumb_up, color: Colors.green),
-                  onPressed: () {
-                    // Handle like button press
-                  },
-                ),
-                IconButton(
-                  icon: const Icon(Icons.thumb_down, color: Colors.green),
-                  onPressed: () {
-                    // Handle unlike button press
-                  },
-                ),
-                IconButton(
-                  icon: const Icon(Icons.comment, color: Colors.green),
-                  onPressed: () {
-                    // Handle comment button press
-                  },
-                ),
-              ],
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.thumb_up, color: Color(0xFF367750)),
+                    onPressed: () {
+                      // Handle like button press
+                    },
+                  ),
+                  IconButton(
+                    icon:
+                        const Icon(Icons.thumb_down, color: Color(0xFF367750)),
+                    onPressed: () {
+                      // Handle unlike button press
+                    },
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.comment, color: Color(0xFF367750)),
+                    onPressed: () {
+                      // Handle comment button press
+                    },
+                  ),
+                ],
+              ),
             ),
           ],
         ),
--- lib/features/writepost.dart
+++ lib/features/writepost.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cc206_human_humay/features/community.dart';
 
-void main() {
-  runApp(const WritePost());
-}
-
 class WritePost extends StatelessWidget {
   const WritePost({super.key});
 
@@ -29,62 +25,96 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Create a Post'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const Community()),
+            );
+          },
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // User Icon and Text Field for Post Content
-            Row(
-              children: [
-                const CircleAvatar(
-                  // You can replace the placeholder image with the user's profile picture
-                  backgroundImage: AssetImage('assets/images/rechiepol.jpeg'),
-                  radius: 20.0,
-                ),
-                const SizedBox(width: 8.0),
-                Expanded(
-                  child: TextFormField(
-                    maxLines: 5,
-                    decoration: const InputDecoration(
-                      hintText: 'What\'s on your mind?',
-                      border: OutlineInputBorder(),
+      body: Container(
+        constraints: const BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF367750),
+              Color(0xFF8B8421),
+            ],
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                children: [
+                  const CircleAvatar(
+                    backgroundImage: AssetImage('assets/images/farmer.png'),
+                    radius: 20.0,
+                  ),
+                  const SizedBox(width: 8.0),
+                  Expanded(
+                    child: TextFormField(
+                      maxLines: 5,
+                      style: const TextStyle(
+                          color: Colors.white), // Set text color to white
+                      decoration: const InputDecoration(
+                        hintText: 'Ask Community',
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.white), // Set border color to white
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors
+                                  .white), // Set focused border color to white
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 16.0),
-
-            // Add Photo Button
-            ElevatedButton.icon(
-              onPressed: () {
-                // Add functionality to pick a photo
-              },
-              icon: const Icon(Icons.add_photo_alternate),
-              label: const Text('Add Photo'),
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
+                ],
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton.icon(
+                onPressed: () {
+                  // Add functionality to pick a photo
+                },
+                icon: const Icon(Icons.add_photo_alternate,
+                    color: Color(0xFF367750)), // Set icon color
+                label: const Text('Add Photo/video',
+                    style:
+                        TextStyle(color: Color(0xFF367750))), // Set text color
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 16.0),
-
-            // Post Button
-            ElevatedButton(
-              onPressed: () {
-                // Add functionality to post
-              },
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Add functionality to post
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  backgroundColor: const Color(0xFF367750),
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 7,
+                      vertical: 10), // Set button background color
                 ),
+                child: const Text('Post',
+                    style: TextStyle(color: Colors.white)), // Set text color
               ),
-              child: const Text('Post'),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
