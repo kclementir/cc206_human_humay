e1c430c89c22df7489e80df20a00b1357550754f
dorknarck <trishanicoleflores97@gmail.com>
2023-12-05 14:48:07 +0800

    update

--- lib/features/writepost.dart
+++ lib/features/writepost.dart
@@ -1,3 +1,4 @@
+import 'package:cc206_human_humay/features/community.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,7 +9,7 @@ class writepost extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Facebook Post',
+      title: 'Ask Community',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
@@ -23,6 +24,17 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Create a Post'),
+        // Add a back button to the AppBar
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            // Replace the line below with your desired functionality
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => Community()),
+            );
+          },
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -34,7 +46,7 @@ class MyHomePage extends StatelessWidget {
               children: [
                 CircleAvatar(
                   // You can replace the placeholder image with the user's profile picture
-                  backgroundImage: AssetImage('assets/images/rechiepol.jpeg'),
+                  backgroundImage: AssetImage('assets/images/rechiepol.jpg'),
                   radius: 20.0,
                 ),
                 SizedBox(width: 8.0),
@@ -42,7 +54,7 @@ class MyHomePage extends StatelessWidget {
                   child: TextFormField(
                     maxLines: 5,
                     decoration: InputDecoration(
-                      hintText: 'What\'s on your mind?',
+                      hintText: 'Ask Community',
                       border: OutlineInputBorder(),
                     ),
                   ),
@@ -57,12 +69,13 @@ class MyHomePage extends StatelessWidget {
                 // Add functionality to pick a photo
               },
               icon: Icon(Icons.add_photo_alternate),
-              label: Text('Add Photo'),
-               style: ElevatedButton.styleFrom(
+              label: Text('Add Photo/video'),
+              style: ElevatedButton.styleFrom(
                 shape: RoundedRectangleBorder(
-                 borderRadius: BorderRadius.circular(10.0),
+                  borderRadius: BorderRadius.circular(10.0),
                 ),
-               ),    
+            
+              ),
             ),
             SizedBox(height: 16.0),
 
@@ -73,14 +86,19 @@ class MyHomePage extends StatelessWidget {
               },
               child: Text('Post'),
               style: ElevatedButton.styleFrom(
+
                 shape: RoundedRectangleBorder(
-                 borderRadius: BorderRadius.circular(10.0),
+                  borderRadius: BorderRadius.circular(10.0),
                 ),
-              ),   
-            ),
+                 padding: EdgeInsets.symmetric(horizontal: 7
+                 , vertical: 10),
+              
+              ),
+              ),
           ],
         ),
       ),
     );
   }
 }
+
