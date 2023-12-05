bd13999ec704bdd0cc78642bf214abd5bbbbe8a7
kclementir <kianiclementir@gmail.com>
2023-12-05 22:43:52 +0800

    updated analyze

--- lib/components/analyze_screen.dart
+++ lib/components/analyze_screen.dart
@@ -1,1 +1,238 @@
+import 'package:flutter/material.dart';
 
+class AnalyzeScreen extends StatelessWidget {
+  const AnalyzeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Crop Analysis',
+          style: TextStyle(
+            fontFamily: 'Brand',
+            color: Color(0xFF367750),
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        iconTheme: const IconThemeData(
+          color: Color(0xFF367750),
+        ),
+      ),
+
+      //Background
+      body: Container(
+        constraints: const BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF367750),
+              Color(0xFF8B8421),
+            ],
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 10),
+              const Text(
+                'Rice Sheath Blight',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              const Text(
+                'Fungus',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+
+              const SizedBox(height: 10),
+              // Container for the picture
+              Container(
+                width: 300,
+                height: 200,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Image.asset(
+                  'assets/images/analyze.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(height: 32),
+
+              // Container for Analyzed Diagnosis
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(16),
+                margin: const EdgeInsets.symmetric(horizontal: 16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Row(
+                  children: [
+                    Icon(
+                      Icons.looks_one,
+                      color: Color(0xFF367750),
+                      size: 32,
+                    ),
+                    SizedBox(width: 16),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Analyzed Diagnosis',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 32),
+
+              // Additional text container
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(16),
+                margin: const EdgeInsets.symmetric(horizontal: 16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: RichText(
+                  text: const TextSpan(
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: 'Symptoms:\n\n',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      TextSpan(
+                        text: 'Leaf-sheath\n',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF367750),
+                        ),
+                      ),
+                      TextSpan(
+                        text:
+                            '    Greenish-gray and oval spots near the water line; later enlarge and become grayish white with brown margin; the lesion spread to the upper leaf sheaths and on leaves that come in contact with infected plant part.\n'
+                            'Leaf\n'
+                            '    Banded brownish lesion with gray-white center; lesions coalesce leading to the blighting of the leaf; affects panicle exertion when the flag leaf is infected.\n\n',
+                      ),
+                      TextSpan(
+                        text: 'Factors that favor the disease:\n',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      TextSpan(
+                        text: '• Susceptible variety\n'
+                            '• Highly humid and warm temperature\n'
+                            '• Frequent rains\n'
+                            '• High rates of nitrogen fertilizer\n'
+                            '• Dense or close planting',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 32),
+              // Container for Analyzed Diagnosis
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(16),
+                margin: const EdgeInsets.symmetric(horizontal: 16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Row(
+                  children: [
+                    Icon(
+                      Icons.looks_two,
+                      color: Color(0xFF367750),
+                      size: 32,
+                    ),
+                    SizedBox(width: 16),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Disease Management',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 32),
+              // Additional text container
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(16),
+                margin: const EdgeInsets.symmetric(horizontal: 16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: RichText(
+                  text: const TextSpan(
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                    children: [
+                      TextSpan(
+                        text:
+                            '• Dry the field after harvest and monitor proper fertilization. Use Leaf Color Chart and Minus-One Element Technique. \n\n'
+                            '• Observe optimum seeding rate; wider plant spacing. \n\n'
+                            '• Assess the field before maximum tillering and panicle emergence. Drain the field at maximum tillering for a few days. \n\n'
+                            '• Use recommended fungicides when needed. Apply on sheaths and affected leaves. \n\n'
+                            '• Ensure proper sanitation. Deep plow the field to bury infected stubble and weeds. Expose soil to intense sunlight.',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 32),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
